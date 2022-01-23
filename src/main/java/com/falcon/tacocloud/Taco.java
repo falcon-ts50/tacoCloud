9a02d2f288e6abd2b921511083d7c2489d084345
falcon <falcon-ts50@yandex.ru>
2022-01-23 03:08:20 +0300

    home page, design - controller

---
+++ src/main/java/com/falcon/tacocloud/Taco.java
@@ -0,0 +1,13 @@
+package com.falcon.tacocloud;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+
+}
