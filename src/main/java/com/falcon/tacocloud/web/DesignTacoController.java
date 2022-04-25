902407fc7e1e88e9456ebe2e2f820bc4e1baac47
falcon <falcon-ts50@yandex.ru>
2022-04-25 18:45:19 +0300

    converter, page with thymeleaf order - controller

--- src/main/java/com/falcon/tacocloud/web/DesignTacoController.java
+++ src/main/java/com/falcon/tacocloud/web/DesignTacoController.java
@@ -6,10 +6,7 @@ import com.falcon.tacocloud.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -56,4 +53,13 @@ public class DesignTacoController {
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
     }
+
+    @PostMapping
+    public String processTaco(Taco taco) {
+// Save the taco...
+// We'll do this in chapter 3
+        log.info("Processing taco: " + taco);
+        return "redirect:/orders/current";
+    }
+
 }
